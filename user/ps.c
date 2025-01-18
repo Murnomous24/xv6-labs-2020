2ee1af7a6385900197e844fb9476a6df95a4fdf0
Murnomous24 <hopkinsesmond@gmail.com>
2025-01-18 15:05:58 +0800

    fix missed git tracked file bug

---
+++ user/ps.c
@@ -0,0 +1,15 @@
+#include "kernel/param.h"
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+    if(argc > 1){
+        fprintf(2, "Usage: ps\n");
+        exit(1);
+    }
+    ps();
+    exit(0);
+}
---
+++ user/pstree.c
@@ -0,0 +1,15 @@
+#include "kernel/param.h"
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+    if(argc > 1){
+        fprintf(2, "Usage: pstree\n");
+        exit(1);
+    }
+    pstree();
+    exit(0);
+}
